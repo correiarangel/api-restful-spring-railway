472a735e386f3a2075caa6ab4809c214e880a02e
correiarangel <correiarangel@bol.com.br>
2024-06-09 14:29:27 -0300

    feat: implementing entities

---
+++ src/main/java/br/com/rangeldev/api_restful_spring_railway/domain/entitties/Feature.java
@@ -0,0 +1,6 @@
+package br.com.rangeldev.api_restful_spring_railway.domain.entitties;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem{}
---
+++ src/main/java/br/com/rangeldev/api_restful_spring_railway/domain/entitties/News.java
@@ -0,0 +1,6 @@
+package br.com.rangeldev.api_restful_spring_railway.domain.entitties;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {}
