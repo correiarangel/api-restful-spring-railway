40102117cac4b08e2d580691fc9e5bbf60b2b675
correiarangel <correiarangel@bol.com.br>
2024-06-09 14:28:40 -0300

    feat: implementing interface serve

---
+++ src/main/java/br/com/rangeldev/api_restful_spring_railway/service/IUserService.java
@@ -0,0 +1,7 @@
+package br.com.rangeldev.api_restful_spring_railway.service;
+
+import br.com.rangeldev.api_restful_spring_railway.domain.entitties.User;
+
+public interface IUserService extends ICrudService<Long, User>  {
+
+}
