b209e346b88436ec13e5bc1ee80742064ed4863f
correiarangel <correiarangel@bol.com.br>
2024-06-09 14:21:59 -0300

    feat: remove renone

---
+++ src/test/java/br/com/rangeldev/api_restful_spring_railway/ApiRestfulSpringRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rangeldev.api_restful_spring_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSpringRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
